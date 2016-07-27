1e5aa9778c9222557a8d560c75cc26362dbe8e46
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2016-07-26 23:48:13 -0700

    Added instance creation to 01_drawtriangles

--- 01_drawtriangle.cpp
+++ 01_drawtriangle.cpp
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <functional>
 #include <memory>
+#include <vector>
 
 #include "vdeleter.hpp"
 
@@ -31,12 +32,62 @@ class HelloTriangleApplication
 
 	void initVulkan()
 	{
+		createInstance();
 	}
 
 	void mainLoop()
 	{
 		while(!glfwWindowShouldClose(window.get())) {
 			glfwPollEvents();
+		}
+	}
+
+	void createInstance()
+	{
+		// Dump the available vulkan extensions
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "Available Vulkan Extensions: " << std::endl;
+		for(const auto& extension : extensions) {
+			std::cout << "\t" << extension.extensionName << std::endl;
+		}
+		std::cout << std::endl;
+
+		// Get the required vulkan extensions for GLFW to function
+		unsigned int glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		std::cout << "GLFW Required Vulkan Extensions: " << std::endl;
+		for(size_t i = 0; i < glfwExtensionCount; i++) {
+			std::cout << "\t" << glfwExtensions[i] << std::endl;
+		}
+		std::cout << std::endl;
+
+		// Setup some simple application information for Vulkan
+		VkApplicationInfo appInfo  = {};
+		appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName   = TITLE;
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName        = "Derpgin";
+		appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion         = VK_API_VERSION_1_0;
+
+		// Setup the instance information
+		VkInstanceCreateInfo createInfo    = {};
+		createInfo.sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo        = &appInfo;
+		createInfo.enabledExtensionCount   = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount       = 0;
+
+		// Create the vulkan instance
+		if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create Vulkan instance");
 		}
 	}
 
